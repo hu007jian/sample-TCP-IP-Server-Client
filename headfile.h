4abf1f2de64047f2adbacdb3aca8420b62f65fb2
hu007jian <hu007jian@hotmail.com>
2014-05-28 22:31:06 +0800

    Create headfile.h

---
+++ headfile.h
@@ -0,0 +1,15 @@
+#ifndef		_H_HEADFILE_H_
+#define		_H_HEADFILE_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <error.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#endif
